44c3e8945285dff31113d7d79deb7ccd15d0c428
sanjines <sara.sanjines@exxis-group.com>
2022-08-19 14:45:32 -0400

    Credenciales

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,9 @@
 <?php 
 
 function conectarDB() : mysqli {
-    $db = new mysqli('localhost', 'root', 'root123', 'bienes_raices');  //'bienesraices_crud'
+    $db = new mysqli('eu-cdbr-west-03.cleardb.net', 'b192b8b803baba', '165419b1', 'heroku_50e75c410e60130');  //'bienesraices_crud'
+
+    //$db = new mysqli('localhost', 'root', 'root123', 'bienes_raices');  //'bienesraices_crud'
 
     if(!$db) {
         echo "Error no se pudo conectar";
